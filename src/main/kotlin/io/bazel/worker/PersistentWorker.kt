c4bb5596dbdca6e7b96e8e06896a8095e89fec4a
Benjamin Lee <ben@ben.cm>
2022-08-24 18:59:16 -0700

    Filter out KSP messages

--- src/main/kotlin/io/bazel/worker/PersistentWorker.kt
+++ src/main/kotlin/io/bazel/worker/PersistentWorker.kt
@@ -114,8 +114,11 @@ class PersistentWorker(
             io.readCapturedAsUtf8String().splitToSequence("\n")
           )
         output = outputLines
+          .asSequence()
           .filter { it.isNotBlank() }
           .filterNot { it.contains("java.correct.class.type.by.place.resolve.scope") }
+          .filterNot { it.contains("[ksp] loaded provider(s): ") }
+          .filterNot { it.contains("[ksp] Found index annotation: ") }
           .joinToString("\n")
           .trim()
         exitCode = status.exit
