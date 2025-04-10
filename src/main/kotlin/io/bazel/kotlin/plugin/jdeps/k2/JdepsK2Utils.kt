34b2f4368f8750db171273bc227667fe34967033
Ben Lee <ben@ben.cm>
2025-04-10 06:46:49 -0700

    Use reflection for access

--- src/main/kotlin/io/bazel/kotlin/plugin/jdeps/k2/JdepsK2Utils.kt
+++ src/main/kotlin/io/bazel/kotlin/plugin/jdeps/k2/JdepsK2Utils.kt
@@ -1,11 +1,42 @@
 package io.bazel.kotlin.plugin.jdeps.k2
 
+import io.bazel.kotlin.plugin.jdeps.k2.RefCache.jbseClass
+import io.bazel.kotlin.plugin.jdeps.k2.RefCache.jbseGetJavaClassMethod
+import io.bazel.kotlin.plugin.jdeps.k2.RefCache.vbseClass
+import io.bazel.kotlin.plugin.jdeps.k2.RefCache.vbseGetVirtualFileMethod
 import org.jetbrains.kotlin.descriptors.SourceElement
-import org.jetbrains.kotlin.fir.java.VirtualFileBasedSourceElement
 import org.jetbrains.kotlin.load.kotlin.JvmPackagePartSource
 import org.jetbrains.kotlin.load.kotlin.KotlinJvmBinarySourceElement
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedContainerSource
+
+private object RefCache {
+
+  val vbseClass: Class<*>? by lazy {
+    runCatching {
+      Class.forName("org.jetbrains.kotlin.fir.java.VirtualFileBasedSourceElement")
+    }.getOrNull()
+  }
+
+  val vbseGetVirtualFileMethod by lazy {
+    vbseClass?.runCatching {
+      getMethod("getVirtualFile")
+    }?.getOrNull()
+  }
+
+  val jbseClass: Class<*>? by lazy {
+    runCatching {
+      Class.forName("org.jetbrains.kotlin.fir.descriptors.impl.JavaBinarySourceElement")
+    }.getOrNull()
+  }
+
+  val jbseGetJavaClassMethod by lazy {
+    jbseClass?.runCatching {
+      getMethod("getJavaClass")
+    }?.getOrNull()
+  }
+}
+
 
 /**
  * Returns whether class is coming from JVM runtime env. There is no need to track these classes.
@@ -25,11 +56,21 @@ internal fun DeserializedContainerSource.classId(): ClassId? =
   }
 
 internal fun SourceElement.binaryClass(): String? =
-  when (this) {
-    is KotlinJvmBinarySourceElement -> binaryClass.location
-    is JvmPackagePartSource -> this.knownJvmBinaryClass?.location
-    is VirtualFileBasedSourceElement -> this.virtualFile.path
-    else -> null
+  if (this is KotlinJvmBinarySourceElement) {
+    binaryClass.location
+  } else if (this is JvmPackagePartSource) {
+    this.knownJvmBinaryClass?.location
+  } else if (vbseClass != null && vbseClass!!.isInstance(this)) {
+    // 1. Try VirtualFileBasedSourceElement (Kotlin 2.1.20+)
+    val virtualFile = vbseGetVirtualFileMethod?.invoke(this)
+    virtualFile?.javaClass?.getMethod("getPath")?.invoke(virtualFile) as? String
+  } else if (jbseClass != null && jbseClass!!.isInstance(this)) {
+    // 2. If that fails, try the old class name: JavaBinarySourceElement
+    val jClass = jbseGetJavaClassMethod?.invoke(this)
+    val virtualFile = jClass?.javaClass?.getMethod("getVirtualFile")?.invoke(jClass)
+    virtualFile?.javaClass?.getMethod("getPath")?.invoke(virtualFile) as? String
+  } else {
+    null
   }
 
 internal fun DeserializedContainerSource.binaryClass(): String? =
